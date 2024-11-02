df96948363d2254051bee430f3e6cd91eb10d09a
EdensNote <whsskhot@gmail.com>
2024-11-02 11:53:05 +0900

    플레이어 컨트롤러 수정 , 에니미 컨트롤러 수정

--- Assets/Scripts/TargetArrow.cs
+++ Assets/Scripts/TargetArrow.cs
@@ -18,12 +18,19 @@ public class TargetArrow : MonoBehaviour
         dir = _targetGlobalPos - transform.position;
         float tempDegree = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
-        if (tempDegree >= 90)
-            tempDegree = 90;
-
-        if (tempDegree <= 0)
-            tempDegree = 0;
-
         transform.localRotation = Quaternion.Euler(0, 0, tempDegree);
+    }
+
+    public Vector3 RotateToTargetAngle(float _angle)
+    {
+        Vector3 startDir = Vector3.right;   //우측을 기준으로 0
+        float tempRadian = _angle * Mathf.Deg2Rad;
+        tempRadian = Mathf.Atan2(startDir.y, startDir.x) + tempRadian;
+        dir = new Vector3(Mathf.Cos(tempRadian), Mathf.Sin(tempRadian), 0);
+
+        transform.localRotation = Quaternion.Euler(0, 0, _angle);
+
+        //회전한 방향 반환
+        return dir;
     }
 }
